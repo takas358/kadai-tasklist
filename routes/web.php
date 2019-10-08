29dbc4b73565df6348e5b45d642f9986c01719b9
takas <masters358@outlook.jp>
2019-10-08 06:03:19 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+
+/*
+  タスクの詳細参照ページ
+  Route::get('tasks/{id}', 'TasksController@show');
+  タスクの新規作成処理
+  Route::post('tasks', 'TasksController@store');
+  タスクの更新処理
+  Route::put('tasks/{id}', 'TasksController@update');
+  タスクの削除処理
+  Route::delete('tasks/{id}', 'TasksController@destroy');
+
+　タスクの一覧参照ページ
+  Route::get('tasks', 'tasksController@index')->name('tasks.index');
+  タスクの新規作成ページ
+  Route::get('tasks/create', 'tasksController@create')->name('tasks.create');
+  タスクの編集ページ
+  Route::get('tasks/{id}/edit', 'tasksController@edit')->name('tasks.edit');
+*/
